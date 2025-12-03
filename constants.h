556ebcc8a8c8cfc87093e77aebbc3bef741a7453
Justme <87026908+Code2-life@users.noreply.github.com>
2025-12-03 15:47:00 -0500

    Add constants for button and servo configurations

---
+++ constants.h
@@ -0,0 +1,32 @@
+#ifndef CONSTANTS
+#define CONSTANTS
+
+constexpr int x = 0; //placeholder
+
+//from ben's file
+
+//Buttons
+constexpr int BUTTON_BOTTOM = 0; //B
+constexpr int BUTTON_RIGHT = 1;  //A
+constexpr int BUTTON_LEFT = 2;   //Y
+constexpr int BUTTON_TOP = 3;    //X
+constexpr int LEFT_BUMPER = 4;
+constexpr int RIGHT_BUMPER = 5;
+constexpr int LEFT_TRIGGER = 6;
+constexpr int RIGHT_TRIGGER = 7;
+constexpr int MID_RIGHT = 9;     //+
+constexpr int MID_LEFT = 8;      //-
+constexpr int L_PRESS = 10;      //L Stick
+constexpr int R_PRESS = 11;      //R Stick
+constexpr int D_UP = 12;
+constexpr int D_DOWN = 13;
+constexpr int D_LEFT = 14;
+constexpr int D_RIGHT = 15;
+//Servo Angles
+
+[blank]
+
+
+
+//
+#endif
